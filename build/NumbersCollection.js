837d37ce65eef68a16ad74ca402fe299c161a3fb
Richard <richard.yang.tong@gmail.com>
2021-04-17 22:36:12 -0400

    Created a new class module NumbersCollection.

---
+++ build/NumbersCollection.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.NumbersCollection = void 0;
+var NumbersCollection = /** @class */ (function () {
+    function NumbersCollection() {
+    }
+    return NumbersCollection;
+}());
+exports.NumbersCollection = NumbersCollection;
+;
