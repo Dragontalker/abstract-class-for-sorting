89be75c9c51ca4e37f58cfb02632423ae73b656b
Richard <richard.yang.tong@gmail.com>
2021-04-18 11:18:23 -0400

    Added comments.

--- build/index.js
+++ build/index.js
@@ -3,12 +3,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var NumbersCollection_1 = require("./NumbersCollection");
 var CharactersCollection_1 = require("./CharactersCollection");
 var LinkedList_1 = require("./LinkedList");
+// Bubble sort for array of numbers.
 var numbersCollection = new NumbersCollection_1.NumbersCollection([50, 3, -5, 0]);
 numbersCollection.sort();
 console.log(numbersCollection.data);
+// Bubble sort for string.
 var charactersCollection = new CharactersCollection_1.CharactersCollection('Xaayb');
 charactersCollection.sort();
 console.log(charactersCollection.data);
+// Bubble sort for linked list.
 var linkedList = new LinkedList_1.LinkedList();
 linkedList.add(500);
 linkedList.add(-10);
--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,17 @@ import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
 import { LinkedList } from './LinkedList';
 
+// Bubble sort for array of numbers.
 const numbersCollection = new NumbersCollection([50, 3, -5, 0]);
 numbersCollection.sort();
 console.log(numbersCollection.data);
 
+// Bubble sort for string.
 const charactersCollection = new CharactersCollection('Xaayb');
 charactersCollection.sort();
 console.log(charactersCollection.data);
 
+// Bubble sort for linked list.
 const linkedList = new LinkedList();
 linkedList.add(500);
 linkedList.add(-10);
