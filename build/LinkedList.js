7cf4bd4fa2f151464bfe65e98be9bb7093d8eecc
Richard <richard.yang.tong@gmail.com>
2021-04-18 10:15:25 -0400

    Added a new add method.

--- build/LinkedList.js
+++ build/LinkedList.js
@@ -12,6 +12,21 @@ var LinkedList = /** @class */ (function () {
     function LinkedList() {
         this.head = null;
     }
+    LinkedList.prototype.add = function (data) {
+        var node = new Node(data);
+        // If there is no Node in head, use it as first element in head.
+        if (!this.head) {
+            this.head = node;
+            return;
+        }
+        // Loop untill the tail.next property is null.
+        var tail = this.head;
+        while (tail.next) {
+            tail = tail.next;
+        }
+        // Link the new Node to the end of linked list.
+        tail.next = node;
+    };
     return LinkedList;
 }());
 exports.LinkedList = LinkedList;
--- src/LinkedList.ts
+++ src/LinkedList.ts
@@ -6,4 +6,23 @@ class Node {
 
 export class LinkedList {
   head: Node | null = null;
+
+  add(data: number) : void {
+    const node = new Node(data);
+
+    // If there is no Node in head, use it as first element in head.
+    if (!this.head) {
+      this.head = node;
+      return;
+    }
+
+    // Loop untill the tail.next property is null.
+    let tail = this.head;
+    while(tail.next) {
+      tail = tail.next
+    }
+
+    // Link the new Node to the end of linked list.
+    tail.next = node;
+  }
 }
