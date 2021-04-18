bf09b3694edaf3ac36cd208cab234bd8cf47ecb5
Richard <richard.yang.tong@gmail.com>
2021-04-18 11:04:03 -0400

    Added an abstract property for length.

--- src/Sorter.ts
+++ src/Sorter.ts
@@ -7,6 +7,7 @@ interface Sortable {
 export abstract class Sorter {
   abstract compare(leftIndex: number, rightIndex: number): boolean;
   abstract swap(leftIndex: number, rightIndex: number): void;
+  abstract length: number;
 
   sort(): void {
     const { length } = this;
